84d1457c16deb7efa34753889d1ea265efe65f6c
Carl Michael Oyam <carloyam21@icloud.com>
2020-09-26 09:20:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, level)
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  def grade(student_grade)
+   roster[student_grade]
+  end
+
+def sort 
+  sorted_list = {}
+  roster.each do |grade, students| #roster key
+    sorted_list[grade] = students.sort #key grade = student values sort
+  end
+  sorted_list #call method
+end
+  
+
+
+
+end
+
+
